d399c7892f9a5f0c767c30abdbff773b03ca294f
Justine C <83780776+JordyKonpyuta@users.noreply.github.com>
2024-10-24 11:07:04 +0200

    Fix can't use face bottom button on widgets

--- Source/DivineConflict/Private/CustomGameInstance.cpp
+++ Source/DivineConflict/Private/CustomGameInstance.cpp
@@ -2,3 +2,10 @@
 
 
 #include "CustomGameInstance.h"
+#include "InputCoreTypes.h"
+
+UCustomGameInstance::UCustomGameInstance()
+{
+	FKey& virtual_key = const_cast<FKey&>(EKeys::Virtual_Accept);
+	virtual_key = EKeys::Invalid;
+}
--- Source/DivineConflict/Public/CustomGameInstance.h
+++ Source/DivineConflict/Public/CustomGameInstance.h
@@ -24,6 +24,7 @@ private:
 /* UFUNCTIONS */	
 public:
 
+	UCustomGameInstance();
 	
 private:
 };
